c46fc7ba873eb19c29a2ed69e143c4b7144ad967
dilshodev3234 <Adilshod3234@gmail.com>
2024-08-12 15:51:54 +0500

    first commit

---
+++ djangoProject1/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+"""
+git config --global user.name "dilshodev3234"
+git config --global user.email "Adilshod3234@gmail.com"
+"""
